c5d921deee0548d6bb765eb159beb53f190e55bc
Elijah Yap <yap@usc.edu>
2021-06-01 15:57:29 -0700

    clone func

--- Teensy_Scripts/buelher-clock/src/ODriveState.cpp
+++ Teensy_Scripts/buelher-clock/src/ODriveState.cpp
@@ -6,6 +6,7 @@
 
 #include "UtilityHelpers.hpp"
 #include "ODriveState.hpp"
+#include "buelher.hpp"
 
 void loop_control(char c, ODriveArduino odrive)
 {
@@ -25,4 +26,47 @@ void idle_state(char c, ODriveArduino odrive)
     Serial << "Axis" << c << ": Requesting state: IDLE (1)" << '\n';
     if (!odrive.run_state(0, requested_state, false))
         return;
+}
+
+void buelher_clock(char c, ODriveArduino odrive, BuelherClock config)
+{
+
+    // MUST enter closed loop mode before starting movement
+    loop_control(c, odrive);
+
+    // max duration in milliseconds
+    const long dur = 50000;
+
+    // flag boolean to kill loop
+    bool cont = true;
+
+    // time string
+    char time[12];
+
+    // track time since loop start
+    long start = millis();
+    long elapsed = millis() - start;
+
+    /**
+             * Loop Until Time Elapses or Q is pressed.
+             */
+    while (cont && elapsed < dur)
+    {
+        elapsed = millis() - start;
+        if (Serial.read() == 'q')
+        {
+            cont = false;
+            continue;
+        }
+
+        float ref_angle = config.getPosition(elapsed, 1);
+        float ref_rots = (1.0f / 360.0f) * ref_angle;
+
+        odrive.SetPosition(0, ref_rots);
+
+        formatTime(time); //gets the time (minutes:seconds:milliseconds)
+        Serial << "| " << elapsed / 1000.0f << "| " << ref_angle << "| " << ref_rots << "\n";
+    }
+    // return motor to idle state on interrupt or completion
+    idle_state(c, odrive);
 }
