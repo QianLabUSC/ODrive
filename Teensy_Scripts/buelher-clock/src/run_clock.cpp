483f3f9bd1d0e4715307d7c0d126a721d2516256
Elijah Yap <yap@usc.edu>
2021-08-05 16:42:08 -0700

    draft

--- Teensy_Scripts/buelher-clock/src/run_clock.cpp
+++ Teensy_Scripts/buelher-clock/src/run_clock.cpp
@@ -8,11 +8,50 @@
 #include "ODriveState.hpp"
 #include "UtilityHelpers.hpp"
 #include "buelher.hpp"
+#include "legs.hpp"
 
 #include "run_clock.hpp"
 
 // max duration in milliseconds
 #define dur 50000
+
+// !Note in progress.
+float getPosition(
+    Leg leg,
+    RoboConfig conf, // physical robot configuration
+    Gait gait,
+    const BuelherClock &clock,
+    long elapsed,
+    int wrap = INT32_MAX)
+{
+
+    float s_elapsed = float(elapsed) / 1000.0f;
+
+    // calculate time offset from leg phase
+    
+
+    // whole number period
+    int rotations = int(s_elapsed / clock.period());
+
+    // fractional period
+    s_elapsed = fmod(s_elapsed, clock.period());
+
+    // whole rotations
+    float angle = rotations * 360.0f;
+
+    // add fractional rotation
+    if (s_elapsed <= clock.time_i())
+        angle += clock.omega_fast() * s_elapsed;
+    else if (s_elapsed <= clock.time_f())
+        angle += clock.theta_i + ((s_elapsed - clock.time_i()) * clock.omega_slow());
+    else
+        angle += clock.theta_f + ((s_elapsed - clock.time_f()) * clock.omega_fast());
+
+    // wrap angle around
+    angle = fmod(angle, float(360 * wrap));
+
+    return angle;
+}
 
 void run_clock(RoboConfig conf, Gait gait, ODriveArduino odrive)
 {
@@ -47,6 +86,10 @@ void run_clock(RoboConfig conf, Gait gait, ODriveArduino odrive)
 
         formatTime(time); //gets the time (minutes:seconds:milliseconds)
         Serial << "| " << elapsed / 1000.0f << "| " << ref_angle << "| " << ref_rots << "\n";
+        
+        // !Note: In Progress
+        float TEMP1 = getPosition(right_fore, conf, gait, EXAMPLE, elapsed);
+        float TEMP2 = getPosition(right_hind, conf, gait, EXAMPLE, elapsed);
     }
 
     // return motor to idle state on interrupt or completion
@@ -56,10 +99,3 @@ void run_clock(RoboConfig conf, Gait gait, ODriveArduino odrive)
 
     Serial << "DONE\n";
 }
-
-// !Note in progress.
-// float getPosition(
-//     RoboConfig conf, // physical robot configuration
-//     Gait gait,
-//     long elapsed,
-//     int wrap = INT32_MAX)
