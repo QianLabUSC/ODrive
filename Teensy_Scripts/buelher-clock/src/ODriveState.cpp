2e15c4ced74e7348b69d77bdc74ec07df31fe5b6
Elijah Yap <yap@usc.edu>
2021-08-05 14:43:47 -0700

    fix hard coded axes

--- Teensy_Scripts/buelher-clock/src/ODriveState.cpp
+++ Teensy_Scripts/buelher-clock/src/ODriveState.cpp
@@ -20,7 +20,7 @@ void loop_control(char c, ODriveArduino odrive)
 
     requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
     Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
-    if (!odrive.run_state(0, requested_state, false /*don't wait*/))
+    if (!odrive.run_state(c, requested_state, false /*don't wait*/))
         return;
 }
 
@@ -30,7 +30,7 @@ void idle_state(char c, ODriveArduino odrive)
 
     requested_state = ODriveArduino::AXIS_STATE_IDLE;
     Serial << "Axis" << c << ": Requesting state: IDLE (1)" << '\n';
-    if (!odrive.run_state(1, requested_state, false))
+    if (!odrive.run_state(c, requested_state, false))
         return;
 }
 
