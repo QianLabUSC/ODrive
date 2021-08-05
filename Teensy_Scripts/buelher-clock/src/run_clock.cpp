ad149775048f1ea051c2df7d9de2e8c9a9addb49
Elijah Yap <yap@usc.edu>
2021-08-05 14:18:52 -0700

    set up test code for 2 motors

--- Teensy_Scripts/buelher-clock/src/run_clock.cpp
+++ Teensy_Scripts/buelher-clock/src/run_clock.cpp
@@ -8,18 +8,24 @@
 #include "ODriveState.hpp"
 #include "UtilityHelpers.hpp"
 #include "buelher.hpp"
+#include "gait.hpp"
 
 #include "run_clock.hpp"
 
 // max duration in milliseconds
 #define dur 50000
 
-void run_clock(char c, ODriveArduino odrive)
+void run_clock(Gait gait, ODriveArduino odrive)
 {
     Serial.println("Executing Buelher Clock. Send 'q' to stop.");
-    loop_control(c, odrive); // MUST enter closed loop mode before starting movement
-    bool cont = true;        // flag boolean to kill loop
-    char time[12];           // time string
+
+    // MUST enter closed loop mode before starting movement
+    // TODO: set all 4 legs
+    loop_control(0, odrive);
+    loop_control(1, odrive);
+
+    bool cont = true; // flag boolean to kill loop
+    char time[12];    // time string
 
     // track time since loop start
     long start = millis();
@@ -38,12 +44,16 @@ void run_clock(char c, ODriveArduino odrive)
         float ref_rots = (1.0f / 360.0f) * ref_angle;
 
         odrive.SetPosition(0, ref_rots);
+        odrive.SetPosition(1, ref_rots);
 
         formatTime(time); //gets the time (minutes:seconds:milliseconds)
         Serial << "| " << elapsed / 1000.0f << "| " << ref_angle << "| " << ref_rots << "\n";
     }
+    
     // return motor to idle state on interrupt or completion
-    idle_state(c, odrive);
+    // TODO: set all 4 legs
+    idle_state(0, odrive);
+    idle_state(1, odrive);
 
     Serial << "DONE\n";
 }
