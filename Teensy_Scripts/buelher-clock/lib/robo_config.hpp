2873a7a94a099e2e3b69881be8a92a410d4e2d65
Elijah Yap <yap@usc.edu>
2021-09-24 12:02:05 -0700

    auto format

--- Teensy_Scripts/buelher-clock/lib/robo_config.hpp
+++ Teensy_Scripts/buelher-clock/lib/robo_config.hpp
@@ -1,28 +1,25 @@
 #include <HardwareSerial.h>
-#include "../lib/ODriveArduino/ODriveArduino.h"
 
+#include "../lib/ODriveArduino/ODriveArduino.h"
 #include "legs.hpp"
 
 #ifndef ROBO_CONFIG_H
 #define ROBO_CONFIG_H
 /**
  * Defines settings for a particular motor on an ODrive robot.
- * !Note: This class reflects a physical / electrical setup, and therefore is highly subject to change.
+ * !Note: This class reflects a physical / electrical setup,
+ * and therefore is highly subject to change.
+ *
  * This source code may change a lot!
  */
-class LegConfig
-{
-public:
-    LegConfig(
-        HardwareSerial odrv,
-        int axis,
-        float init_offset,
-        bool gyre);
+class LegConfig {
+   public:
+    LegConfig(HardwareSerial odrv, int axis, float init_offset, bool gyre);
 
     // Address a specific motor
     // TODO: get actual HardwareSerial and ODriveArduino object!
-    HardwareSerial odrv; // which ODrive board is being addressed
-    int axis;            // which axis (0 or 1) on that board is being addressed
+    HardwareSerial odrv;  // which ODrive board is being addressed
+    int axis;  // which axis (0 or 1) on that board is being addressed
 
     /**
      * Initial angular offset for a neutral / zero position.
@@ -32,7 +29,8 @@ public:
      */
     float init_offset;
 
-    // Indicates whether the motor should rotate normally, or in the opposite direction.
+    // Indicates whether the motor should rotate normally, or in the opposite
+    // direction.
     bool invert_direction;
 };
 
@@ -41,16 +39,12 @@ public:
  * of a 4 legged 1 DOF ODrive Robot.
  * Assumptions: we are using Serial 1 and Serial 2 on the Teensy Arduino.
  */
-class RoboConfig
-{
-public:
+class RoboConfig {
+   public:
     static const int MOTOR_COUNT = 4;
 
-    RoboConfig(
-        LegConfig right_fore,
-        LegConfig left_fore,
-        LegConfig right_hind,
-        LegConfig left_hind);
+    RoboConfig(LegConfig right_fore, LegConfig left_fore, LegConfig right_hind,
+               LegConfig left_hind);
 
     // Option to look up a leg using its enum.
     // !Note: Read Only!
@@ -72,11 +66,11 @@ public:
 // !Note: Define Configurations Here:
 
 // 21.08.12 testing config with 2 motors
-const RoboConfig CONFIG = RoboConfig(
-    LegConfig(Serial1, 0, 0.0f, false), // right_fore
-    LegConfig(Serial1, 1, 0.0f, true),  // left_fore
-    LegConfig(Serial2, 0, 0.0f, false), // right_hind
-    LegConfig(Serial2, 1, 0.0f, true)   // left_hind
-);
+const RoboConfig CONFIG =
+    RoboConfig(LegConfig(Serial1, 0, 0.0f, false),  // right_fore
+               LegConfig(Serial1, 1, 0.0f, true),   // left_fore
+               LegConfig(Serial2, 0, 0.0f, false),  // right_hind
+               LegConfig(Serial2, 1, 0.0f, true)    // left_hind
+    );
 
 #endif
--- Teensy_Scripts/buelher-clock/src/robo_config.cpp
+++ Teensy_Scripts/buelher-clock/src/robo_config.cpp
@@ -1,50 +1,37 @@
 #include "robo_config.hpp"
 
-RoboConfig::RoboConfig(
-    LegConfig right_fore,
-    LegConfig left_fore,
-    LegConfig right_hind,
-    LegConfig left_hind) : right_fore(right_fore),
-                           left_fore(left_fore),
-                           right_hind(right_hind),
-                           left_hind(left_hind)
-{
-}
+RoboConfig::RoboConfig(LegConfig right_fore, LegConfig left_fore,
+                       LegConfig right_hind, LegConfig left_hind)
+    : right_fore(right_fore),
+      left_fore(left_fore),
+      right_hind(right_hind),
+      left_hind(left_hind) {}
 
-void run_config()
-{
-    
-}
+void run_config() {}
 
-LegConfig RoboConfig::operator[](const Leg &leg)
-{
-    switch (leg)
-    {
-    case Leg::right_fore:
-        return right_fore;
-        break;
-    case Leg::left_fore:
-        return left_fore;
-        break;
-    case Leg::right_hind:
-        return right_hind;
-        break;
-    case Leg::left_hind:
-        return left_hind;
-        break;
+LegConfig RoboConfig::operator[](const Leg &leg) {
+    switch (leg) {
+        case Leg::right_fore:
+            return right_fore;
+            break;
+        case Leg::left_fore:
+            return left_fore;
+            break;
+        case Leg::right_hind:
+            return right_hind;
+            break;
+        case Leg::left_hind:
+            return left_hind;
+            break;
     };
 
     // Should never exit without hitting enum!
     exit(1);
 }
 
-LegConfig::LegConfig(
-    HardwareSerial odrv,
-    int axis,
-    float init_offset,
-    bool invert_direction) : odrv(odrv),
-                             axis(axis),
-                             init_offset(init_offset),
-                             invert_direction(invert_direction)
-{
-}
+LegConfig::LegConfig(HardwareSerial odrv, int axis, float init_offset,
+                     bool invert_direction)
+    : odrv(odrv),
+      axis(axis),
+      init_offset(init_offset),
+      invert_direction(invert_direction) {}
